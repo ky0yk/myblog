647c12805a6326c20118659358fc9dbdff936690
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-17 21:32:50 +0900

    UserエンティティにVOを反映

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,13 +1,34 @@
-export class User {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+import { Email } from "../vo/Email";
+import { Name } from "../vo/Name";
+import { Password } from "../vo/Password";
+import { UserId } from "../vo/UserId";
 
-    constructor(id: string, name: string, email: string, password: string) {
+export class User {
+    private id: UserId;
+    private name: Name;
+    private email: Email;
+    private password: Password;
+
+    constructor(id: UserId, name: Name, email: Email, password: Password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    getId(): UserId {
+        return this.id;
+    }
+
+    getName(): Name {
+        return this.name;
+    }
+
+    getEmail(): Email {
+        return this.email;
+    }
+
+    getPassword(): Password {
+        return this.password;
     }
 }
