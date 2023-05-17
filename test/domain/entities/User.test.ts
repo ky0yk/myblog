41cadac3cdd4d4b8347e148e80ab54774d9d8144
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-17 22:23:47 +0900

    エンティティのテストを追加

--- test/domain/entities/User.test.ts
+++ test/domain/entities/User.test.ts
@@ -2,36 +2,20 @@ import { User } from "../../../src/domain/entities/User";
 import { Email } from "../../../src/domain/vo/Email";
 import { Name } from "../../../src/domain/vo/Name";
 import { Password } from "../../../src/domain/vo/Password";
-import { UserId } from "../../../src/domain/vo/UserId"
+import { UserId } from "../../../src/domain/vo/UserId";
 
 describe('User', () => {
-    const userId = new UserId('1234');
-    const userName = new Name('John Doe');
-    const userEmail = new Email('johndoe@example.com');
-    const userPassword = new Password('password123');
-    const user = new User(userId, userName, userEmail, userPassword);
+    it('should create a user successfully', () => {
+      const user = new User(
+        new UserId('1'),
+        new Name('Test User'),
+        new Email('test@example.com'),
+        new Password('password'),
+      );
 
-    describe('getId', () => {
-        it('should return the id of the user', () => {
-            expect(user.getId().getValue()).toBe('1234');
-        });
+      expect(user.getId().getValue()).toEqual('1');
+      expect(user.getName().getValue()).toEqual('Test User');
+      expect(user.getEmail().getValue()).toEqual('test@example.com');
+      expect(user.getPassword().getValue()).toEqual('password');
     });
-
-    describe('getName', () => {
-        it('should return the name of the user', () => {
-            expect(user.getName().getValue()).toBe('John Doe');
-        });
-    });
-
-    describe('getEmail', () => {
-        it('should return the email of the user', () => {
-            expect(user.getEmail().getValue()).toBe('johndoe@example.com');
-        });
-    });
-
-    describe('getPassword', () => {
-        it('should return the password of the user', () => {
-            expect(user.getPassword().getValue()).toBe('password123');
-        });
-    });
-})
+  });
