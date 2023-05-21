d397a51085f9a9c7ea32fbaa52fa198fdcb5f407
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-21 17:41:11 +0900

    インフラ層のUserRepositoryを実装

---
+++ src/infrastructure/repositories/UserRepository.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { UserId } from "../../domain/vo/UserId";
+import { User } from "../../domain/entities/User";
+import { Name } from "../../domain/vo/Name";
+import { Email } from "../../domain/vo/Email";
+import { Password } from "../../domain/vo/Password";
+
+export class UserRepository implements IUserRepository {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    async find(id: UserId): Promise<User | null> {
+        const result = await this.prisma.user.findUnique({ where: {id: id.value} });
+
+        if (result == null) {
+            return null;
+        }
+
+        return new User(
+            new UserId(result.id),
+            new Name(result.name),
+            new Email(result.email),
+            new Password(result.password)
+        );
+    }
+
+    async findByEmail(email: Email): Promise<User | null> {
+        const result = await this.prisma.user.findUnique({ where: { email: email.value}})
+
+        if (result == null) {
+            return null;
+        }
+
+        return new User(
+            new UserId(result.id),
+            new Name(result.name),
+            new Email(result.email),
+            new Password(result.password)
+        );
+    }
+
+
+    async save(user: User): Promise<void> {
+        const {id , name, email, password } = user;
+
+        const result = await this.prisma.user.upsert({
+            where: {id: id.value},
+            update: {name: name.value, email: email.value, password: password.value},
+            create: { id: id.value, name: name.value, email: email.value, password: password.value }
+        });
+    }
+
+}
