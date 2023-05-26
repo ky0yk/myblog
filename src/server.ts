e92b9c4f9b7d7c2382f73c690a8714da84d3a5d8
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-26 20:46:42 +0900

    server.tsに依存関係を追加し、UserRouterを使用するように設定

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { UserRepository } from "./infrastructure/repositories/UserRepository";
+import { UserUseCase } from "./application/usecases/UserUseCase";
+import { UserController } from "./presentation/controller/UserController";
+import { UserRouter } from "./presentation/router.ts/UserRouter";
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const prisma = new PrismaClient();
+const userRepository = new UserRepository(prisma);
+const userUseCase = new UserUseCase(userRepository);
+const userController = new UserController(userUseCase);
+const userRouter = new UserRouter(userController);
+
+
+app.use("/user", userRouter.router);
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
