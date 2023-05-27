dc1a3b73a5768e5d29f16999a1083b3fa64a6707
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-27 12:49:32 +0900

    Userのコントローラーとルーターを追加

---
+++ src/presentation/Router/UserRouter.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { UserController } from '../Controller/UserController';
+
+export class UserRouter {
+    private userController: UserController;
+    public router = express.Router();
+
+    constructor(userController: UserController) {
+        this.userController = userController;
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() {
+        this.router.post('/', (req, res) => this.userController.createUser(req, res));
+        this.router.get('/:userId', (req, res) => this.userController.getUser(req, res));
+        this.router.put('/:userId', (req, res) => this.userController.updateUser(req, res));
+        this.router.delete('/:userId', (req, res) => this.userController.deleteUser(req, res));
+    }
+}
