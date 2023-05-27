d31eecfb67a563251368efefeb027f653b766065
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-27 13:42:38 +0900

    Postへのルーティングを設定

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,27 @@
 import express from 'express';
 import { UserController } from './presentation/Controller/UserController';
 import { UserRouter } from './presentation/Router/UserRouter';
+import { PostController } from './presentation/Controller/PostController';
+import { PostRouter } from './presentation/Router/PostRouter';
 
 const app = express();
 const port = 3000;
 
 const userController = new UserController();
+const userRouter = new UserRouter(userController);
+
+const postController = new PostController();
+const postRouter = new PostRouter(postController);
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const userRouter = new UserRouter(userController);
+
 app.use('/users', userRouter.router);
+
+app. use('/posts', postRouter.router);
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
