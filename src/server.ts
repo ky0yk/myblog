7e5aa9600975f6ab9891a2fd4e02809f4e61f707
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-27 12:49:48 +0900

    Userのルーターを反映

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import { UserController } from './presentation/Controller/UserController';
+import { UserRouter } from './presentation/Router/UserRouter';
 
 const app = express();
 const port = 3000;
 
+const userController = new UserController();
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+const userRouter = new UserRouter(userController);
+app.use('/users', userRouter.router);
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
