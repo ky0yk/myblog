bd7d70816180e353befeb522220ecf594959a1a8
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-28 10:10:15 +0900

    AuthControllerのloginメソッドの更新

--- src/presentation/controller/AuthController.ts
+++ src/presentation/controller/AuthController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from 'express';
+import { AuthService } from '../../application/AuthService';
+import { Email } from '../../domain/vo/Email';
+import { Password } from '../../domain/vo/Password';
 
 export class AuthController {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    constructor(){}
+    private authService: AuthService;
+
+    constructor(authService: AuthService){
+        this.authService = authService;
+    }
 
     async login(req: Request, res: Response): Promise<void> {
-        res.json({message: "auth"})
+        try {
+            const email = new Email(req.body.email);
+            const password = new Password(req.body.password);
+            const token = await this.authService.login(email, password);
+
+            if (token) {
+                res.json({ token });
+            } else {
+                res.json(401).json({ message: 'Invalid email or password'});
+            }
+        } catch(err) {
+            res.status(400).json({ error: err instanceof Error ? err.message : String(err) });
+        }
     }
 }
