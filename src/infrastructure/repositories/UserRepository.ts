27d356f1c6b46ac7c5f0daa5df5dc029f16c794e
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-05-28 10:54:14 +0900

    パスワードを保存時にハッシュ化する様に変更

--- src/infrastructure/repositories/UserRepository.ts
+++ src/infrastructure/repositories/UserRepository.ts
@@ -5,7 +5,7 @@ import { User } from "../../domain/entities/User";
 import { Name } from "../../domain/vo/Name";
 import { Email } from "../../domain/vo/Email";
 import { Password } from "../../domain/vo/Password";
-
+import { hashPassword } from "../../utils/passwordHasher";
 
 export class UserRepository implements IUserRepository {
     private _prisma: PrismaClient;
@@ -47,11 +47,12 @@ export class UserRepository implements IUserRepository {
 
     async save(user: User): Promise<User> {
         const {id , name, email, password } = user;
+        const hashedPassword = await hashPassword(password);
 
         const result = await this._prisma.user.upsert({
             where: {id: id.value},
-            update: {name: name.value, email: email.value, password: password.value},
-            create: { id: id.value, name: name.value, email: email.value, password: password.value }
+            update: {name: name.value, email: email.value, password: hashedPassword},
+            create: { id: id.value, name: name.value, email: email.value, password: hashedPassword}
         });
 
         return new User(new UserId(result.id), new Name(result.name), new Email(result.email), new Password(result.password));
