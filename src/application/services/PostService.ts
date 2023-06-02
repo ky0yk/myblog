be305a593d0be23d9414418ea0bc0fe1ef4fcde9
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-06-02 21:00:06 +0900

    レスポンス用のdtoを作成・追加

--- src/application/services/PostService.ts
+++ src/application/services/PostService.ts
@@ -6,29 +6,41 @@ import { Title } from '../../domain/vo/Title';
 import { UserId } from '../../domain/vo/UserId';
 import { PostRepository } from '../../infrastructure/repositories/PostRepository'
 import { PostCreateDto } from '../dto/post/PostCreateDto';
+import { PostResponseDto } from '../dto/post/PostResonseDto';
 import { PostUpdateDto } from '../dto/post/PostUpdateDto';
 import { v4 as uuidv4 } from "uuid";
-
 
 export class PostService {
   constructor(private postRepository: PostRepository) {
     this.postRepository = postRepository;
   }
 
-  async getAllPosts(): Promise<Post[]> {
-    const posts = await this.postRepository.findAll();
-    return posts;
+  private toDto(post: Post): PostResponseDto {
+    return {
+      id: post.id.value,
+      authorId: post.authorId.value,
+      title: post.title.value,
+      content: post.content.value,
+      isPublished: post.isPublished,
+      createdAt: post.createdAt?.toISOString() ?? '',
+      updatedAt: post.updatedAt?.toISOString() ?? ''
+    };
   }
 
-  async getPostById(id: PostId): Promise<Post> {
+  async getAllPosts(): Promise<PostResponseDto[]> {
+    const posts = await this.postRepository.findAll();
+    return posts.map(this.toDto);
+  }
+
+  async getPostById(id: PostId): Promise<PostResponseDto> {
     const post = await this.postRepository.findById(id);
     if (!post) {
       throw new Error('Post not found');
     }
-    return post;
+    return this.toDto(post);
   }
 
-  async createPost(postDto: PostCreateDto): Promise<Post> {
+  async createPost(postDto: PostCreateDto): Promise<PostResponseDto> {
     const post = new Post(
       new PostId(uuidv4()),
       new Title(postDto.title),
@@ -36,10 +48,10 @@ export class PostService {
       new UserId(postDto.authorId),
     );
     const savedPost = await this.postRepository.save(post);
-    return savedPost;
+    return this.toDto(savedPost);
   }
 
-  async updatePost(id: PostId, postDto: PostUpdateDto): Promise<Post> {
+  async updatePost(id: PostId, postDto: PostUpdateDto): Promise<PostResponseDto> {
     const post = await this.postRepository.findById(id);
     if (!post) {
       throw new Error('Post not found');
@@ -48,7 +60,7 @@ export class PostService {
     const updatedPost = post.updateWithDto(postDto);
     const savedPost = await this.postRepository.save(updatedPost);
 
-    return savedPost;
+    return this.toDto(savedPost);
   }
 
   async deletePost(id: PostId): Promise<void> {
