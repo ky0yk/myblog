a97fed7e1a1a13709451a0fb101d6954b6cf2a20
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-06-04 16:42:31 +0900

    エンティティを更新

--- src/domain/entities/Comment.ts
+++ src/domain/entities/Comment.ts
@@ -1,13 +1,15 @@
-import { CommentBody } from '../vo/CommentBody'
-import { CommentId } from '../vo/CommentId'
-import { PostId } from '../vo/PostId'
-import { UserId } from '../vo/UserId'
+import { CommentBody } from "../vo/CommentBody";
+import { CommentId } from "../vo/CommentId";
+import { PostId } from "../vo/PostId";
+import { UserId } from "../vo/UserId";
 
 export class Comment {
-  private _id: CommentId
-  private _body: CommentBody
-  private _authorId: UserId
-  private _postId: PostId
+  private _id: CommentId;
+  private _body: CommentBody;
+  private _authorId: UserId;
+  private _postId: PostId;
+  private _createdAt?: Date;
+  private _updatedAt?: Date;
 
   constructor(
     id: CommentId,
@@ -15,25 +17,44 @@ export class Comment {
     authorId: UserId,
     postId: PostId
   ) {
-    this._id = id
-    this._body = body
-    this._authorId = authorId
-    this._postId = postId
+    this._id = id;
+    this._body = body;
+    this._authorId = authorId;
+    this._postId = postId;
+    // _createdAt and _updatedAt will be set when the instance is saved to the database.
   }
 
-  get id() {
-    return this._id
+  // Getters
+  get id(): CommentId {
+    return this._id;
   }
 
-  get body() {
-    return this._body
+  get body(): CommentBody {
+    return this._body;
   }
 
-  get authorId() {
-    return this._authorId
+  get authorId(): UserId {
+    return this._authorId;
   }
 
-  get postId() {
-    return this._postId
+  get postId(): PostId {
+    return this._postId;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  get updatedAt() {
+    return this._updatedAt;
+  }
+
+  // Setters
+  setCreatedAt(createdAt: Date): void {
+    this._createdAt = createdAt;
+  }
+
+  setUpdatedAt(updatedAt: Date): void {
+    this._updatedAt = updatedAt;
   }
 }
